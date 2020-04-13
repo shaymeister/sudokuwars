391544c3f0d098758e00560af00dcf888665f474
Shay Snyder <shaymeister@hotmail.com>
2020-04-13 16:29:46 -0400

    finished creating base framework

---
+++ src/sudoku/Settings.java
@@ -0,0 +1,162 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: Settings.java
+ * Project name: SudokuWars
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Holden Dalton, daltonh@etsu.edu
+ * Creator's name and email: Shay Snyder, snyderse2@etsu.edu
+ * Creator's name and email: Hannah Taylor, hannahm1@mail.etsu.edu
+ *
+ * Course:  CSCI 1260-288
+ * Creation Date: April 13, 2020
+ * ---------------------------------------------------------------------------
+ */
+
+package sudoku;
+
+/**
+ * Manage and control all settings within SudokuWars
+ *
+ * <hr>
+ * Date created: April 13, 2020
+ * Last modified: April 13, 2020
+ * <hr>
+ * @author Holden Dalton
+ * @author Shay Snyder
+ * @author Hannah Taylor
+ */
+public class Settings
+{
+    // global variables
+    private int masterVolume;
+    private int effectsVolume;
+    private int melodyVolume;
+    private int screenHeight;
+    private int screenWidth;
+
+    /**
+	 * initialize the Settings class to the default values
+     *
+	 * <hr>
+	 * Date created: April 13, 2020
+	 */
+    public Settings()
+    {
+        // TODO FINISH IMPLEMENTATION
+    } // END: Settings() no-arg constructor
+
+    /**
+	 * TODO Finish Documentation
+     *
+	 * <hr>
+	 * Date created: April 13, 2020
+	 */
+    public void setMasterVolume()
+    {
+        // TODO FINISH IMPLEMENTATION
+    } // END: setMasterVolume() method
+
+    /**
+	 * TODO Finish Documentation
+     *
+	 * <hr>
+	 * Date created: April 13, 2020
+	 */
+    public void setEffectsVolume()
+    {
+        // TODO FINISH IMPLEMENTATION
+    } // END: setEffectsVolume() method
+
+    /**
+	 * TODO Finish Documentation
+     *
+	 * <hr>
+	 * Date created: April 13, 2020
+	 */
+    public void setMelodyVolume()
+    {
+        // TODO FINISH IMPLEMENTATION
+    } // END: setMelodyVolume() method
+
+    /**
+	 * TODO Finish Documentation
+     *
+	 * <hr>
+	 * Date created: April 13, 2020
+	 */
+    public void setScreenHeight()
+    {
+        // TODO FINISH IMPLEMENTATION
+    } // END: setScreenHeight() method
+
+    /**
+	 * TODO Finish Documentation
+     *
+	 * <hr>
+	 * Date created: April 13, 2020
+	 */
+    public void setScreenWidth()
+    {
+        // TODO FINISH IMPLEMENTATION
+    } // END: setScreenWidth() method
+
+    /**
+	 * TODO Finish Documentation
+     *
+	 * <hr>
+	 * Date created: April 13, 2020
+	 */
+    public int getMasterVolume()
+    {
+        // TODO FINISH IMPLEMENTATION
+        return 0;
+    } // END: getMasterVolume() method
+
+    /**
+	 * TODO Finish Documentation
+     *
+	 * <hr>
+	 * Date created: April 13, 2020
+	 */
+    public int getEffectsVolume()
+    {
+        // TODO FINISH IMPLEMENTATION
+        return 0;
+    } // END: getEffectsVolume() method
+
+    /**
+	 * TODO Finish Documentation
+     *
+	 * <hr>
+	 * Date created: April 13, 2020
+	 */
+    public int getMelodyVolume()
+    {
+        // TODO FINISH IMPLEMENTATION
+        return 0;
+    } // END: getMasterVolume() method
+
+    /**
+	 * TODO Finish Documentation
+     *
+	 * <hr>
+	 * Date created: April 13, 2020
+	 */
+    public int getScreenHeight()
+    {
+        // TODO FINISH IMPLEMENTATION
+        return 0;
+    } // END: getScreenHeight() method
+
+    /**
+	 * TODO Finish Documentation
+     *
+	 * <hr>
+	 * Date created: April 13, 2020
+	 */
+    public int getScreenWidth()
+    {
+        // TODO FINISH IMPLEMENTATION
+        return 0;
+    } // END: getScreenWidth() method
+} // END: Settings() class
