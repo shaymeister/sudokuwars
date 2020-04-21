a7e4b0520b24e8051af6dc10f364cdfcc0eb6de7
HannahMTaylor <taylorhm2001@gmail.com>
2020-04-21 17:21:32 -0400

    updated Board.java (but not finished, just saving progress)

--- src/sudoku/Board.java
+++ src/sudoku/Board.java
@@ -16,6 +16,8 @@ package sudoku;
 
 import java.util.Calendar;
 import java.util.Random;
+import java.io.*;
+import java.util.*;
 
 /**
  * Represent and control the sudoku playing surface
@@ -41,18 +43,246 @@ public class Board
     private long endTime;
     private int numOfMoves;
     private int score;
+	private String boardFilePath;
 
+	
+	
     /**
-	 * TODO FINISH DOCUMENTATION
+	 * import the board file into the array for manipulation
+	 * to play the game
      *
 	 * <hr>
 	 * Date created: April 13, 2020
 	 */
-    public void importBoard(Difficulty difficulty)
+    public void importBoard()
     {
-        // TODO FINISH IMPLEMENTATION
+		File inputFile = new File(boardFilePath);
+		Scanner inputRead = null;
+		try
+		{
+			inputRead = new Scanner(inputFile);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Error reading file: " + e.getMessage());
+		}
+		
+		while(inputRead.hasNext())
+		{
+			//hold current line
+			String str = inputRead.nextLine();
+			String[] fields = str.split("\\|"); //split line into fields
+			int p;
+			
+			//initialize the Piece[][]
+			for (int r=0; r < 9; r++)
+			{
+				for (int c=0; c < 9; c++)
+				{
+					p = Integer.parseInt(fields[c]);
+					
+					//set the enum based on the int in the field
+					if(p == 0)
+						this.board[r][c] = Piece.EMPTY;
+					else if(p == 1)
+						this.board[r][c] = Piece.ONE;
+					else if(p == 2)
+						this.board[r][c] = Piece.TWO;
+					else if(p == 3)
+						this.board[r][c] = Piece.THREE;
+					else if(p == 4)
+						this.board[r][c] = Piece.FOUR;
+					else if(p == 5)
+						this.board[r][c] = Piece.FIVE;
+					else if(p == 6)
+						this.board[r][c] = Piece.SIX;
+					else if(p == 7)
+						this.board[r][c] = Piece.SEVEN;
+					else if(p == 8)
+						this.board[r][c] = Piece.EIGHT;
+					else if(p == 9)
+						this.board[r][c] = Piece.NINE;
+				}
+				
+			}
+		}
+        
+		
     } // END: importBoard() method
-
+	
+	/**
+	* determine the board to import and set the path
+	* 
+	* <hr>
+	* Date created: April 21, 2020
+	*
+	* @param difficulty
+	*/
+	public void getBoardToUse(Difficulty difficulty)
+	{
+		// based on the difficulty chosen, get a random board of that level
+		//and initialize it to board[][]
+		int rand = rnd.nextInt(10);
+		switch (difficulty)
+		{
+			case EASY:
+			{
+				switch(rand)
+				{
+					case 0:
+						boardFilePath = "easy1.txt";
+						break;
+					case 1:
+						boardFilePath = "easy2.txt";
+						break;
+					case 2:
+						boardFilePath = "easy3.txt";
+						break;
+					case 3:
+						boardFilePath = "easy4.txt";
+						break;
+					case 4:
+						boardFilePath = "easy5.txt";
+						break;
+					case 5:
+						boardFilePath = "easy6.txt";
+						break;
+					case 6:
+						boardFilePath = "easy7.txt";
+						break;
+					case 7:
+						boardFilePath = "easy8.txt";
+						break;
+					case 8:
+						boardFilePath = "easy9.txt";
+						break;
+					case 9:
+						boardFilePath = "easy10.txt";
+						break;
+				}
+				break;
+			}
+			case MEDIUM:
+			{
+				switch(rand)
+				{
+					case 0:
+						boardFilePath = "medium1.txt";
+						break;
+					case 1:
+						boardFilePath = "medium2.txt";
+						break;
+					case 2:
+						boardFilePath = "medium3.txt";
+						break;
+					case 3:
+						boardFilePath = "medium4.txt";
+						break;
+					case 4:
+						boardFilePath = "medium5.txt";
+						break;
+					case 5:
+						boardFilePath = "medium6.txt";
+						break;
+					case 6:
+						boardFilePath = "medium7.txt";
+						break;
+					case 7:
+						boardFilePath = "medium8.txt";
+						break;
+					case 8:
+						boardFilePath = "medium9.txt";
+						break;
+					case 9:
+						boardFilePath = "medium10.txt";
+						break;
+				}
+				break;
+			}
+			case HARD:
+			{
+				switch(rand)
+				{
+					case 0:
+						boardFilePath = "hard1.txt";
+						break;
+					case 1:
+						boardFilePath = "hard2.txt";
+						break;
+					case 2:
+						boardFilePath = "hard3.txt";
+						break;
+					case 3:
+						boardFilePath = "hard4.txt";
+						break;
+					case 4:
+						boardFilePath = "hard5.txt";
+						break;
+					case 5:
+						boardFilePath = "hard6.txt";
+						break;
+					case 6:
+						boardFilePath = "hard7.txt";
+						break;
+					case 7:
+						boardFilePath = "hard8.txt";
+						break;
+					case 8:
+						boardFilePath = "hard9.txt";
+						break;
+					case 9:
+						boardFilePath = "hard10.txt";
+						break;
+				}
+				break;
+			}
+			case EXTREME:
+			{
+				switch(rand)
+				{
+					case 0:
+						boardFilePath = "extreme1.txt";
+						break;
+					case 1:
+						boardFilePath = "extreme2.txt";
+						break;
+					case 2:
+						boardFilePath = "extreme3.txt";
+						break;
+					case 3:
+						boardFilePath = "extreme4.txt";
+						break;
+					case 4:
+						boardFilePath = "extreme5.txt";
+						break;
+					case 5:
+						boardFilePath = "extreme6.txt";
+						break;
+					case 6:
+						boardFilePath = "extreme7.txt";
+						break;
+					case 7:
+						boardFilePath = "extreme8.txt";
+						break;
+					case 8:
+						boardFilePath = "extreme9.txt";
+						break;
+					case 9:
+						boardFilePath = "extreme10.txt";
+						break;
+				}
+				break;
+			}
+			case UNBEATABLE:
+				boardFilePath = "imossibleBoard.txt";
+				break;
+			
+			
+			
+		}
+		
+	}// end getBoardToUse()
+	
     /**
 	 * TODO FINISH DOCUMENTATION
      *
