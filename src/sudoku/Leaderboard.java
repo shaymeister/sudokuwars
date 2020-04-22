1778410a204dcda571decb571e00bf0fb16fc6b2
holdendalton10 <holdendalton10@gmail.com>
2020-04-22 12:43:42 -0400

    Changes to leaderboard

--- src/sudoku/Leaderboard.java
+++ src/sudoku/Leaderboard.java
@@ -15,6 +15,10 @@
 package sudoku;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
+import javax.swing.*;
+
 
 /**
  * Contain an manage all previous boards
@@ -41,8 +45,48 @@ public class Leaderboard
 	 */
     public Leaderboard()
     {
-        // TODO FINISH IMPLEMENTATION
-    } // END: Leaderboard() no-arg constructor
+        this.leaderboard = new ArrayList<Board>();
+		
+		try 
+		{
+			File leaderText = new File("leaderboard.txt");
+			Scanner input = null;
+			
+			if(leaderText.exists())
+			{
+				input = new Scanner(leaderText);
+			}
+			
+			while(input.hasNext())
+			{
+				String strTextLine = input.nextLine();
+				String[] fields = strTextLine.split("\\|");
+				
+				String strUser = fields[0];
+				String strDifficulty = fields[1];
+				String strTime = fields[2];
+				String strMoves = fields[3];
+				
+				Difficulty diff = Difficulty.valueOf(strDifficulty);
+				
+				Board board = new Board(strUser,diff);
+				this.leaderboard.add(board);
+			}
+			input.close();
+		}
+		
+		catch(Exception ex)
+		{
+			JOptionPane.showMessageDialog(null,"Error from the leaderboard constructor","Sudoku",JOptionPane.ERROR_MESSAGE);
+		}
+		
+    } // END: Leaderboard()
+
+	public void addToList(Board board)
+	{
+		this.leaderboard.add(board);
+	}
+
 
     /**
 	 * TODO FINISH DOCUMENTATION
@@ -52,7 +96,27 @@ public class Leaderboard
 	 */
     public void sortByPlayer()
     {
-        // TODO FINISH IMPLEMENTATION
+        String user1;
+		String user2;
+		Board board1;
+		Board board2;
+		
+		for(int counter=0;counter<leaderboard.size();counter++)
+		{
+			for(int position=counter+1;position<leaderboard.size();position++)
+			{
+				board1 = leaderboard.get(counter);
+				board2 = leaderboard.get(position);
+				user1 = board1.getUser();
+				user2 = board2.getUser();
+				if(user1.compareTo(user2)>0)
+				{
+					leaderboard.set(counter, user2);
+					leaderboard.set(position, user1);
+				}
+			}
+		}
+		
     } // END: sortByPlayer() method
 
     /**
@@ -63,7 +127,32 @@ public class Leaderboard
 	 */
     public void sortByDifficulty()
     {
-        // TODO FINISH IMPLEMENTATION
+        Difficulty diff1;
+		Difficulty diff2;
+		String strDiff1;
+		String strDiff2;
+		Board board1;
+		Board board2;
+		
+		for(int counter=0;counter<leaderboard.size();counter++)
+		{
+			for(int position=counter+1;position<leaderboard.size();position++)
+			{
+				board1 = leaderboard.get(counter);
+				board2 = leaderboard.get(position);
+				diff1 = board1.getDifficulty();
+				diff2 = board2.getDifficulty();
+				
+				strDiff1 = diff1.toString();
+				strDiff2 = diff2.toString();
+				
+				if(strDiff1.compareTo(strDiff2)>0)
+				{
+					leaderboard.set(counter, board2);
+					leaderboard.set(position, board1);
+				}
+			}
+		}
     } // END: sortByDifficulty() method
 
     /**
@@ -74,19 +163,8 @@ public class Leaderboard
 	 */
     public void sortByTime()
     {
-        // TODO FINISH IMPLEMENTATION
+        
     } // END: sortByTime() method
-
-    /**
-	 * TODO FINISH DOCUMENTATION
-     *
-	 * <hr>
-	 * Date created: April 13, 2020
-	 */
-    public void sortByScore()
-    {
-        // TODO FINISH IMPLEMENTATION
-    } // END: sortByScore() method
 
     /**
 	 * TODO FINISH DOCUMENTATION
@@ -96,7 +174,28 @@ public class Leaderboard
 	 */
     public void sortByMoves()
     {
-        // TODO FINISH IMPLEMENTATION
+        String moves1;
+		String moves2;
+		Board board1;
+		Board board2;
+		
+		for(int counter=0;counter<leaderboard.size();counter++)
+		{
+			for(int position=counter+1;position<leaderboard.size();position++)
+			{
+				board1 = leaderboard.get(counter);
+				board2 = leaderboard.get(position);
+				moves1 = board1.getNumOfMoves();
+				moves2 = board2.getNumOfMoves();
+				
+				if(moves1.compareTo(moves2)>0)
+				{
+					leaderboard.set(counter, board2);
+					leaderboard.set(position, board1);
+				}
+			}
+		}
+		
     } // END: sortByMoves() method
 
     /**
@@ -105,10 +204,21 @@ public class Leaderboard
 	 * <hr>
 	 * Date created: April 13, 2020
 	 */
-    public ArrayList<Board> searchByPlayer()
+    public ArrayList<Board> searchByPlayer(String user)
     {
-        // TODO FINISH IMPLEMENTATION
-        return null; //! change later
+        ArrayList<Board> byPlayer = new ArrayList<Board>();
+		Board board;
+		String strUser;
+		for(int counter=0;counter<leaderboard.size();counter++)
+		{
+			board = leaderboard.get(counter);
+			strUser = board.getUser();
+			if(strUser.equals(user))
+			{
+				byPlayer.add(board);
+			}
+		}
+		return byPlayer;
     } // END: searchByPlayer() method
 
     /**
@@ -117,47 +227,23 @@ public class Leaderboard
 	 * <hr>
 	 * Date created: April 13, 2020
 	 */
-    public ArrayList<Board> searchByDifficulty()
+    public ArrayList<Board> searchByDifficulty(Difficulty diff)
     {
-        // TODO FINISH IMPLEMENTATION
-        return null; //! change later
+        ArrayList<Board> diffBoard;
+		Difficulty difficult;
+		Board board;
+		for(int counter=0;counter<leaderboard.size();counter++)
+		{
+			board = leaderboard.get(counter);
+			difficult = board.getDifficulty();
+			if(difficult.equals(diff))
+			{
+				diffBoard.add(board);
+			}
+		}
+		return diffBoard;
     } // END: searchByDifficulty() method
 
-    /**
-	 * TODO FINISH DOCUMENTATION
-     *
-	 * <hr>
-	 * Date created: April 13, 2020
-	 */
-    public ArrayList<Board> searchByTime()
-    {
-        // TODO FINISH IMPLEMENTATION
-        return null; //! change later
-    } // END: searchByTime() method
-
-    /**
-	 * TODO FINISH DOCUMENTATION
-     *
-	 * <hr>
-	 * Date created: April 13, 2020
-	 */
-    public ArrayList<Board> searchByScore()
-    {
-        // TODO FINISH IMPLEMENTATION
-        return null; //! change later
-    } // END: searchByScore() method
-
-    /**
-	 * TODO FINISH DOCUMENTATION
-     *
-	 * <hr>
-	 * Date created: April 13, 2020
-	 */
-    public ArrayList<Board> searchByMoves()
-    {
-        // TODO FINISH IMPLEMENTATION
-        return null; //! change later
-    } // END: searchByMoves() method
 
     /**
 	 * TODO FINISH DOCUMENTATION
