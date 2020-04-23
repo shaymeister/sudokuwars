9a162f84ea65d3f066be08380bb3ff64ae94115c
holdendalton10 <holdendalton10@gmail.com>
2020-04-23 15:35:09 -0400

    Updated comments

--- src/sudoku/Board.java
+++ src/sudoku/Board.java
@@ -4,6 +4,8 @@
  * Project name: SudokuWars
  * ---------------------------------------------------------------------------
  * Creator's name and email: Shay Snyder, snyderse2@etsu.edu
+ *							 Holden Dalton, daltonh@etsu.edu
+ *							 Hannah Taylor, taylorhm1@mail.etsu.edu
  *
  * Course:  CSCI 1260-288
  * Creation Date: April 13, 2020
@@ -28,6 +30,8 @@ import java.util.Scanner;
  * Last modified: April 13, 2020
  * <hr>
  * @author Shay Snyder
+ * @author Holden Dalton
+ * @author Hannah Taylor 
  */
 public class Board
 {
